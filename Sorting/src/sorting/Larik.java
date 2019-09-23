033ad1cad3b4ad58adfd77397f76e01a6bf5b26b
Nx <Nx@YOGA>
2019-09-23 13:38:43 +0700

    minggu depan uts kampank

--- Sorting/src/sorting/Larik.java
+++ Sorting/src/sorting/Larik.java
@@ -20,14 +20,41 @@ public class Larik {
 
     public static void bubleSort(int[] larik) {
         for (int iterasi = 0; iterasi < larik.length; iterasi++) {
-            for (int elemen = 0; elemen < larik.length-1-iterasi; elemen++) {
+            for (int elemen = 0; elemen < larik.length - 1 - iterasi; elemen++) {
                 if (larik[elemen] > larik[elemen + 1]) {
                     int temp;
                     temp = larik[elemen];
                     larik[elemen] = larik[elemen + 1];
-                    larik[elemen+1] = temp;                    
-                }                
+                    larik[elemen + 1] = temp;
+                }
             }
+        }
+    }
+
+    public static void selectionSort(int[] larik) {
+        for (int i = 0; i < larik.length; i++) {
+            int minIndex = i;
+            for (int elemen = i + 1; elemen < larik.length; elemen++) {
+                if (larik[elemen] < larik[minIndex]) {
+                    minIndex = elemen;
+                }
+            }
+            int temp;
+            temp = larik[minIndex];
+            larik[minIndex] = larik[i];
+            larik[i] = temp;
+        }
+    }
+    public static void insertionSort(int[] larik){
+        for (int i = 1; i <= larik.length-1; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (larik[j]<larik[j-1]) {
+                    int temp;
+                    temp = larik[j];
+                    larik[j]= larik[j-1];
+                    larik[j-1] =temp;                
+                }                
+            }            
         }
     }
 }
--- Sorting/src/sorting/Main.java
+++ Sorting/src/sorting/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] data = {4,6,8,3,2};
-        Larik.bubleSort(data);
+        Larik.insertionSort(data);
         Larik.larik(data);
 
     }
