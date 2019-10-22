7a87b0234f5958d6a560505bbd8bb09536214b49
Nx <Nx@YOGA>
2019-10-22 13:19:03 +0700

    Update quick Sort

--- Sorting/src/sorting/Larik.java
+++ Sorting/src/sorting/Larik.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sorting;
 
 /**
@@ -18,14 +13,14 @@ public class Larik {
         }
     }
 
-    public static void bubleSort(int[] larik) {
-        for (int iterasi = 0; iterasi < larik.length; iterasi++) {
-            for (int elemen = 0; elemen < larik.length - 1 - iterasi; elemen++) {
-                if (larik[elemen] > larik[elemen + 1]) {
+    public static void insertionSort(int[] larik) {
+        for (int i = 1; i <= larik.length - 1; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (larik[j] < larik[j - 1]) {
                     int temp;
-                    temp = larik[elemen];
-                    larik[elemen] = larik[elemen + 1];
-                    larik[elemen + 1] = temp;
+                    temp = larik[j];
+                    larik[j] = larik[j - 1];
+                    larik[j - 1] = temp;
                 }
             }
         }
@@ -45,16 +40,56 @@ public class Larik {
             larik[i] = temp;
         }
     }
-    public static void insertionSort(int[] larik){
-        for (int i = 1; i <= larik.length-1; i++) {
-            for (int j = i; j >= 1; j--) {
-                if (larik[j]<larik[j-1]) {
+
+    public static void bubleSort(int[] larik) {
+        for (int iterasi = 0; iterasi < larik.length; iterasi++) {
+            for (int elemen = 0; elemen < larik.length - 1 - iterasi; elemen++) {
+                if (larik[elemen] > larik[elemen + 1]) {
                     int temp;
-                    temp = larik[j];
-                    larik[j]= larik[j-1];
-                    larik[j-1] =temp;                
-                }                
-            }            
+                    temp = larik[elemen];
+                    larik[elemen] = larik[elemen + 1];
+                    larik[elemen + 1] = temp;
+                }
+            }
         }
+
     }
+
+    public static void quickSort(int[] larik, int awal, int akhir) {
+
+        if (awal < akhir) {
+            int i = awal + 1;
+            int j = akhir;
+
+            while (i <= akhir && larik[i] <= larik[awal]) {
+                i++;
+            }
+            while (j > awal && larik[j] > larik[awal]) {
+                j--;
+            }
+            while (i < j) {
+                int temp;
+                temp = larik[i];
+                larik[i] = larik[j];
+                larik[j] = temp;
+
+                while (i <= akhir && larik[i] <= larik[awal]) {
+                    i++;
+                }
+                while (j > awal && larik[j] > larik[awal]) {
+                    j--;
+                }
+            }
+            int temp;
+            temp = larik[awal];
+            larik[awal] = larik[j];
+            larik[j] = temp;
+
+            quickSort(larik, awal, j - 1);
+            quickSort(larik, j+1, akhir);
+
+        }
+
+    }
+
 }
